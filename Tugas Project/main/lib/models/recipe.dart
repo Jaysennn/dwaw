56f3b940be20ffdf86928533167f0319a563d763
Jaysennn <jaysenstephanus_2327250042@mhs.mdp.ac.id>
2024-11-23 21:56:35 +0700

    ppp

--- Tugas Project/main/lib/models/recipe.dart
+++ Tugas Project/main/lib/models/recipe.dart
@@ -1,19 +1,22 @@
 class Recipe {
+  final String id;
   final String name;
   final List<String> category;
-  final List<String> description;
+  final String description;
   final List<String> alat;
   final List<String> bahan;
   final List<String> langkahlangkah;
   final String imageAsset;
 
-  Recipe({
-  required this.name, 
-  required this.category, 
-  required this.description, 
-  required this.alat, 
-  required this.bahan,
-  required this.langkahlangkah, 
-  required this.imageAsset
+  Recipe( 
+    {
+      required this.id,
+    required this.name,
+    required this.category,
+    required this.description,
+    required this.alat,
+    required this.bahan,
+    required this.langkahlangkah,
+    required this.imageAsset,
   });
 }
